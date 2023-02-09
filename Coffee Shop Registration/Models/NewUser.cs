f8f6f0684a7f8d1bd9b70dcb98a157cde4494d8e
Tee <sotomi.tayo@gmail.com>
2023-02-09 15:41:44 -0600

    Coffee shop First Submit without Extra Challenges

---
+++ Coffee Shop Registration/Models/NewUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Coffee_Shop_Registration.Models
+{
+    public class NewUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
